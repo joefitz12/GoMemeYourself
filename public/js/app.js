dabf8470b5a928f822be25b0fbeccd600d1ee5c3
Mina Slater <slater.mina@gmail.com>
2018-03-07 13:45:15 -0600

    adds listener to start game that hits post new game route

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
-$("#start-game").on("click", function(){
-    console.log("start button clicked");
-});
+$("#start-game").on("click", createNewGame);
+
+function createNewGame(){
+  console.log("start button clicked");
+  let gameData = {round: 0}
+  $.post("/games/new", gameData)
+    .then(function(data) {
+      console.log(data);
+    });
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,17 @@
-//API Routes//
+var db = require("../models");
 
-// app.get("/reservations", function(req, res){
-//     res.json(reservations);
-// });
+module.exports = function (app) {
+  // app.get("/reservations", function (req, res) {
+  //   res.json(reservations);
+  // });
 
-// app.get("/waitinglist", function(req,res){
-//     res.json(waitinglist);
-// });
+  // app.get("/waitinglist", function (req, res) {
+  //   res.json(waitinglist);
+  // });
 
-// app.post("/reservations/new", function(req, res){
-//     var newReservation = req.body;
-//     console.log(newReservation);
-//     if (reservations.length > 4){
-//         waitingList.push(newReservation);
-//     }
-//     else {
-//         reservations.push(newReservation);
-//     }
-    
-//     res.json(newReservation);
-// });
+  app.post("/games/new", function (req, res) {
+    db.Game.create(req.body).then(function(response) {
+      res.json(response);
+    });
+  });
+}
