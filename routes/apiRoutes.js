97dac85b0661fd97bdc068071ef474f3232721c3
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-03-08 17:29:17 -0600

    adds object to table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -44,35 +44,66 @@ module.exports = function (app) {
     //     });
     // });
 
-    app.post('/api/photos/new', function(req, res){
-
-        console.log("api route hit!");
+    app.post('/api/photos/new', function (req, res) {
 
         // create an incoming form object
         var form = new formidable.IncomingForm();
-      
+        let fileName = "";
+        let gameID = 0;
+        let playerID = 0;
+        let roundNumber = 0;
+
+        // for (var pair of form.entries()) {
+        //     console.log(pair[0] + ', ' + pair[1]);
+        // }
+
         // store all uploads in the /uploads directory
         form.uploadDir = path.join(__dirname, '../public/photos');
-      
+
+
         // every time a file has been uploaded successfully,
         // rename it to it's orignal name
-        form.on('file', function(field, file) {
-          fs.rename(file.path, path.join(form.uploadDir, file.name));
+        form.on('file', function (field, file) {
+            console.log("field:", field);
+            console.log("file.name:", file.name);
+            fileName = file.name;
+            fs.rename(file.path, path.join(form.uploadDir, file.name));
         });
-      
+
+        form.on('field', function(name, value) {
+            if (name === "gameID"){
+                gameID = value;
+            }
+            else if (name === "playerID"){
+                playerID = value;
+            }
+            else if (name === "roundNumber"){
+                roundNumber = value;
+            }
+        });
+
+        db.Photo.create({
+            GameId: parseInt(gameID),
+            PlayerId: parseInt(playerID),
+            round: parseInt(roundNumber),
+            location: "photos/" + fileName,
+        }).then(()=>callback(null));
+
         // log any errors that occur
-        form.on('error', function(err) {
-          console.log('An error has occured: \n' + err);
+        form.on('error', function (err) {
+            console.log('An error has occured: \n' + err);
         });
-      
+
         // once all the files have been uploaded, send a response to the client
-        form.on('end', function() {
-          res.end('success');
+        form.on('end', function () {
+            res.end('success');
         });
-      
+
+        console.log("form", form);
+
         // parse the incoming request containing the form data
         form.parse(req);
-      
+
     });
 
     app.post("/games/new", function (req, res) {
