e887a6fcd452d3b3a28605af988bc77608a41f12
Joe Fitzpatrick <joe@dhcp-10-102-178-146.wireless.northwestern.private>
2018-03-12 10:23:25 -0500

    adds phone-vote.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -17,4 +17,8 @@ module.exports = function(app){
     app.get("/phone-caption/*/*/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/phone-caption.html"));
     });
+
+    app.get("/phone-vote", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/phone-vote.html"));
+    });
 };
