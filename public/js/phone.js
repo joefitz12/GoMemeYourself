c03281fa0ceb5a2dc674215413030ffa012ab06d
Joe Fitzpatrick <joe@dhcp-10-102-178-146.wireless.northwestern.private>
2018-03-13 15:50:45 -0500

    removes progress bar xhr

--- public/js/phone.js
+++ public/js/phone.js
@@ -52,34 +52,34 @@ $("#photo-submit").on('click touchstart', function () {
         contentType: false,
         success: function (data) {
           console.log('upload successful!\n' + data);
-        },
-        xhr: function () {
-          // create an XMLHttpRequest
-          var xhr = new XMLHttpRequest();
-
-          // listen to the 'progress' event
-          xhr.upload.addEventListener('progress', function (evt) {
-
-            if (evt.lengthComputable) {
-              // calculate the percentage of upload completed
-              var percentComplete = evt.loaded / evt.total;
-              percentComplete = parseInt(percentComplete * 100);
-
-              // update the Bootstrap progress bar with the new percentage
-              $('.progress-bar').text(percentComplete + '%');
-              $('.progress-bar').width(percentComplete + '%');
-
-              // once the upload reaches 100%, set the progress bar text to done
-              if (percentComplete === 100) {
-                $('.progress-bar').html('Done');
-              }
-
-            }
-
-          }, false);
-
-          return xhr;
         }
+        // xhr: function () {
+        //   // create an XMLHttpRequest
+        //   var xhr = new XMLHttpRequest();
+
+        //   // listen to the 'progress' event
+        //   xhr.upload.addEventListener('progress', function (evt) {
+
+        //     if (evt.lengthComputable) {
+        //       // calculate the percentage of upload completed
+        //       var percentComplete = evt.loaded / evt.total;
+        //       percentComplete = parseInt(percentComplete * 100);
+
+        //       // update the Bootstrap progress bar with the new percentage
+        //       $('.progress-bar').text(percentComplete + '%');
+        //       $('.progress-bar').width(percentComplete + '%');
+
+        //       // once the upload reaches 100%, set the progress bar text to done
+        //       if (percentComplete === 100) {
+        //         $('.progress-bar').html('Done');
+        //       }
+
+        //     }
+
+        //   }, false);
+
+        //   return xhr;
+        // }
       }).then(location.replace("/phone-caption/gameID=" + gameID + "/playerID=" + playerID + "/roundNumber=" + roundNumber + "/"));
     }
   }
