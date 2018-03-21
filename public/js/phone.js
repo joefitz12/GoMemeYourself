74d500bba5aab24115617116022bb15f52239ddf
Joe Fitzpatrick <joe@dhcp-10-102-178-146.wireless.northwestern.private>
2018-03-21 09:46:27 -0500

    updates font size

--- public/js/phone.js
+++ public/js/phone.js
@@ -164,19 +164,6 @@ $("#caption-submit").on("click", function (event) {
   }
 });
 
-// $(document).on("click", ".meme-submission", function () {
-//   if (!$(".selected")) {
-//     $(this).addClass("selected");
-//   }
-//   else if ($(this).hasClass("selected")) {
-//     $(".selected").removeClass("selected");
-//   }
-//   else {
-//     $(".selected").removeClass("selected");
-//     $(this).addClass("selected");
-//   }
-// });
-
 $("#vote-submit").on("click", function () {
   let photoID = parseInt($(".selected").attr("data-photoID"));
   if (photoID) {
